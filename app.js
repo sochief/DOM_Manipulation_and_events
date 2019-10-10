712b6dd8f1f3226f2941a341c979c780454e0f67
Chief <shafkatoo@gmail.com>
2019-10-10 15:03:32 +0300

    Zeros at the beginning

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ roundScore = 0;
 activePlayer = 1;
 
 document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
 // Now we set up a Roll dice button
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
