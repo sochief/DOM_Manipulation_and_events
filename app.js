ce0a5a59b6c28748157d34d449075280566f2eb4
Chief <shafkatoo@gmail.com>
2019-10-11 17:45:18 +0300

    10.11

--- app.js
+++ app.js
@@ -9,16 +9,7 @@ GAME RULES:
 
 */
 var scores, roundScore, activePlayer, dice;
-scores = [0, 0];
-roundScore = 0;
-
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 // Now we set up a Roll dice button
 
@@ -81,4 +72,24 @@ function nextPlayer() {
     document.querySelector('.dice').style.display = 'none';
 
     // so for more clear code and if the IF statement is very simple you can use turnary operator instead of writing full if-else statement
+}
+document.querySelector('.btn-new').addEventListener('click', init);
+function init() {
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    // assure, that there is no active class, and then activate 1, so there will be no bugs
+    document.querySelector('.player-0-panel').classList.add('active');
 }
